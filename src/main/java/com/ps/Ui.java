56975891bd486a3064275b0865ac5584dc6e832d
unknown <you@example.com>
2024-11-13 13:15:32 -0600

    second commit, rough skeleton of all classes besides interface

---
+++ src/main/java/com/ps/Ui.java
@@ -0,0 +1,16 @@
+package com.ps;
+
+import com.ps.customClasses.Order;
+
+import java.util.Scanner;
+
+public class Ui {
+    private static Order order = new Order();
+
+    private static Scanner commandScanner = new Scanner(System.in);
+    private static Scanner inputScanner = new Scanner(System.in);
+
+    public static void diplay(){
+
+    }
+}
---
+++ src/main/java/com/ps/customClasses/Product.java
@@ -0,0 +1,4 @@
+package com.ps.customClasses;
+
+public abstract class Product  {
+}
